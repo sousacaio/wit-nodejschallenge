eac6035f21e2354cd1d2561f9a672a6dfcabc736
caio frias <caio.sousa@extreme.digital>
2022-01-23 19:26:12 -0300

    feat: add middlewares to sum route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { endTime } from './adapters/endTime';
+import { myLogger } from './adapters/logger';
 import { IParams } from './entities/Params/IParams';
 import { Params } from './entities/Params/Params';
-import { makeLogOperationController } from './useCases/LogOperation';
-// rest of the code remains same
+
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
-app.post('/sum', (req, res) => {
+export interface RequestCustom extends Request {
+  logger: typeof myLogger;
+  startAt: any;
+  endTime: typeof endTime;
+}
 
+app.use((req: RequestCustom, _res: Response, next: NextFunction) => {
+  req.logger = myLogger
+  req.startAt = process.hrtime()
+  req.endTime = endTime
+  next()
+})
+
+app.get('/', (req: RequestCustom, res) => {
+  console.log(req.endTime(req.startAt))  
+  res.send('Tchau')
+})
+
+app.post('/sum', (req: RequestCustom, res: Response) => {
   let { a, b } = req.query
 
   let parameters: IParams = {
@@ -22,8 +39,18 @@ app.post('/sum', (req, res) => {
   if (paramsOrError.isLeft()) {
     return res.status(400).send(paramsOrError.value)
   }
+  
+  //salvar os dados no banco
 
-  makeLogOperationController().handle(parameters)
+  const info = {
+    clientIp: req.ip,
+    orderId: 1,
+    executionTime: req.endTime(req.startAt),
+    statusCode: 200
+  };
+  
+  req.logger.log('info', info)
+
   res.json('nice')
 });
 
