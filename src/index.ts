45668a1b9c51f8a88f3520a9b0a5a21a50b1c758
caio frias <caio.sousa@extreme.digital>
2022-01-29 17:51:21 -0300

    feat: add cors to express and parse params

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { endTime } from './adapters/endTime';
 import { myLogger } from './adapters/logger';
 import { IParams } from './entities/Params/IParams';
 import { Params } from './entities/Params/Params';
-
+import cors from 'cors'
 const app = express();
 const PORT = 8000;
 
@@ -12,7 +12,8 @@ export interface RequestCustom extends Request {
   startAt: any;
   endTime: typeof endTime;
 }
-
+app.use(cors())
+app.use(express.json())
 app.use((req: RequestCustom, _res: Response, next: NextFunction) => {
   req.logger = myLogger
   req.startAt = process.hrtime()
@@ -21,39 +22,44 @@ app.use((req: RequestCustom, _res: Response, next: NextFunction) => {
 })
 
 app.get('/', (req: RequestCustom, res) => {
-  console.log(req.endTime(req.startAt))  
+  console.log(req.endTime(req.startAt))
   res.send('Tchau')
 })
 
 app.post('/sum', (req: RequestCustom, res: Response) => {
-  let { a, b } = req.query
+  try {
+    console.log(req.body)
+    let { a, b } = req.body
+    
+    let parameters: IParams = {
+      a: parseInt(a),
+      b: parseInt(b),
+      clientIp: req.ip,
+      date: Date.now()
+    }
+    
+    const paramsOrError = Params.create(parameters)
+    
+    if (paramsOrError.isLeft()) {    
+      return res.status(400).send(paramsOrError.value)
+    }
+    
+    //salvar os dados no banco
 
-  let parameters: IParams = {
-    a,
-    b,
-    clientIp: req.ip,
-    date: Date.now()
+    const info = {
+      clientIp: req.ip,
+      orderId: 1,
+      executionTime: req.endTime(req.startAt),
+      statusCode: 200
+    };
+
+    req.logger.log('info', info)    
+    res.json('nice')
+  } catch (error) {
+    console.log(error)
   }
-  const paramsOrError = Params.create(parameters)
-
-  if (paramsOrError.isLeft()) {
-    return res.status(400).send(paramsOrError.value)
-  }
-  
-  //salvar os dados no banco
-
-  const info = {
-    clientIp: req.ip,
-    orderId: 1,
-    executionTime: req.endTime(req.startAt),
-    statusCode: 200
-  };
-  
-  req.logger.log('info', info)
-
-  res.json('nice')
 });
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
